64699e65475441ad1f282364de01ec67074788f3
jhonenmanuel1 <88171380+jhonenmanuel1@users.noreply.github.com>
2022-04-04 17:42:23 -0400

    segundo

---
+++ src/components/header.js
@@ -0,0 +1,15 @@
+const headerView = ()=>{
+
+    return(
+        <div>
+            <ul>
+                <li> 00 HOME</li>
+                <li> 01 DESTINATION </li>
+                <li> 02 CREW </li>
+                <li> 03 TECHNOLOGY</li>
+
+            </ul>
+
+        </div>
+    )
+}
